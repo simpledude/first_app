737835c65355f6d70b9a65c91de72d6fe1834e2d
TANWEER <tanweer@tanweer-laptop.(none)>
2011-09-14 11:26:55 +0500

    "Initial first commit"

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Test1myaql::Application.config.secret_token = '31cdf26ad49da18e02465e5929ad01a40c4170570623d68f0b7405ab08f6f34cd4699085e7169a6af8bd781a9bf65f5a968fb1cb176509a46bf0aaec1e11d321'
